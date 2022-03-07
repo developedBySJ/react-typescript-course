ea6c6c6c8a286641ccbcf41d681e9e9f481494ae
developedBySJ <developedbysj@gmail.com>
2022-03-07 22:56:33 +0530

    Finish L3 milestone

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppContainer } from "./AppContainer";
 import { Form } from "./Form";
 import { Header } from "./Header";
+import { ListForms } from "./ListForms";
 
 function App() {
   const [page, setPage] = React.useState("HOME");
-  const openForm = () => setPage("FORM");
-  const closeForm = () => setPage("HOME");
+  const [curFormId, setCurFormId] = useState<number>(-1);
+  const openForm = (id: number) => setCurFormId(id);
+  const closeForm = () => setCurFormId(-1);
 
   return (
     <AppContainer>
       <div className="w-full max-w-md px-4 rounded-2xl bg-gray-900">
         <Header title="Welcome to Dark Theme" />
 
-        {page === "HOME" ? (
-          <button onClick={openForm} className="btn mb-8 mx-4">
-            Go to Form
-          </button>
+        {curFormId > 0 ? (
+          <Form id={curFormId} onClose={closeForm} />
         ) : (
-          <Form onClose={closeForm} />
+          <>
+            <ListForms openForm={openForm} />
+            <button
+              className="btn my-6 mx-4"
+              onClick={() => openForm(new Date().getTime())}
+            >
+              New Form
+            </button>
+          </>
         )}
       </div>
     </AppContainer>
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,56 +1,151 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface FormProps {
+  id: number;
   onClose: () => void;
 }
 
-const initialFormField = [
+interface FormField {
+  id: number;
+  label: string;
+  type: string;
+  value: string;
+}
+
+interface FormData {
+  id: number;
+  title: string;
+  formFields: FormField[];
+}
+
+export const FORM_DATA_KEY = "formData";
+
+const initialFormField: FormField[] = [
   { id: 1, label: "First Name", type: "text", value: "" },
   { id: 2, label: "Last Name", type: "text", value: "" },
   { id: 3, label: "Email", type: "email", value: "" },
   { id: 4, label: "Date of Birth", type: "date", value: "" },
 ];
 
-export const Form: React.FC<FormProps> = ({ onClose }) => {
-  const [formFields, setFormFields] = useState(initialFormField);
+export const getForms = () => {
+  const rawForms = localStorage.getItem(FORM_DATA_KEY);
+  const forms = (rawForms ? JSON.parse(rawForms) : []) as FormData[];
+  return forms;
+};
+
+const saveToLocalStorage = (formField: FormData) => {
+  const localForms = getForms();
+
+  const filteredLocalForms = localForms.filter(
+    (form) => form.id !== formField.id
+  );
+
+  localStorage.setItem(
+    FORM_DATA_KEY,
+    JSON.stringify([...filteredLocalForms, formField])
+  );
+};
+
+const getFromLocalStorage = (id: number): FormData => {
+  const forms = getForms();
+
+  const existingForm = forms.find((form) => form.id === id);
+
+  return existingForm
+    ? existingForm
+    : {
+        id,
+        title: "Untitled Form",
+        formFields: initialFormField,
+      };
+};
+
+export const Form: React.FC<FormProps> = ({ onClose, id }) => {
+  const [formData, setFormData] = useState(() => getFromLocalStorage(id));
   const [newField, setNewField] = React.useState("");
+  const titleRef = useRef<HTMLInputElement>(null);
 
   const handleNewFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewField(e.target.value);
   };
 
   const handleNewFieldAddClick = () => {
-    setFormFields((prev) => [
+    setFormData((prev) => ({
       ...prev,
-      {
-        id: new Date().getTime(),
-        label: newField,
-        type: "text",
-        value: "",
-      },
-    ]);
+      formFields: [
+        ...prev.formFields,
+        {
+          id: new Date().getTime(),
+          label: newField,
+          type: "text",
+          value: "",
+        },
+      ],
+    }));
     setNewField("");
   };
 
   const handleInputFieldRemoveClick = (id: number) => () => {
-    setFormFields(formFields.filter((field) => field.id !== id));
+    setFormData((prev) => ({
+      ...prev,
+      formFields: prev.formFields.filter((field) => field.id !== id),
+    }));
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      title: e.target.value,
+    }));
   };
 
   const handleClearFormClick = () => {
-    setFormFields((prev) => prev.map((field) => ({ ...field, value: "" })));
+    setFormData((prev) => ({
+      ...prev,
+      formFields: prev.formFields.map((field) => ({ ...field, value: "" })),
+    }));
   };
+
+  useEffect(() => {
+    const tId = setTimeout(() => {
+      saveToLocalStorage(formData);
+    }, 1000);
+
+    return () => {
+      saveToLocalStorage(formData);
+      clearTimeout(tId);
+    };
+  }, [formData]);
+
+  useEffect(() => {
+    const prevTitle = document.title;
+    document.title = "Form Editor";
+    titleRef.current?.focus();
+    return () => {
+      document.title = prevTitle;
+    };
+  }, []);
+
   return (
     <div>
+      <input
+        type="text"
+        className="input"
+        value={formData.title}
+        ref={titleRef}
+        onChange={handleTitleChange}
+      />
       <div className="border-b border-gray-700 py-2 mb-4">
-        {formFields.map(({ id, label, type, value }) => {
+        {formData.formFields.map(({ id, label, type, value }) => {
           const handleInputValueChange = (
             e: React.ChangeEvent<HTMLInputElement>
           ) => {
-            setFormFields((prev) =>
-              prev.map((field) =>
+            setFormData((prev) => ({
+              ...prev,
+              formFields: prev.formFields.map((field) =>
                 field.id === id ? { ...field, value: e.target.value } : field
-              )
-            );
+              ),
+            }));
           };
           return (
             <div key={id} className="flex flex-col my-2 mb-4">
@@ -89,15 +184,24 @@ export const Form: React.FC<FormProps> = ({ onClose }) => {
         </div>
       </div>
 
-      <div className="flex gap-4">
-        <button className="btn bg-gray-800 mb-8" onClick={onClose}>
+      <div className="flex gap-2 justify-between">
+        <button className="btn flex-1 bg-gray-800 mb-8 p-2" onClick={onClose}>
           Close
         </button>
-        <button className="btn bg-gray-800 mb-8" onClick={handleClearFormClick}>
+        <button
+          className="btn bg-gray-800 mb-8 p-2 flex-1"
+          onClick={handleClearFormClick}
+        >
           Clear
         </button>
+        <button
+          className="btn bg-green-500 mb-8 p-2 flex-1"
+          onClick={(_) => saveToLocalStorage(formData)}
+        >
+          Save
+        </button>
 
-        <button className="btn mb-8">Submit</button>
+        <button className="btn mb-8 p-2 flex-1">Submit</button>
       </div>
     </div>
   );
